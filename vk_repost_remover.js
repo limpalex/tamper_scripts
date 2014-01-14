81fa76ee73585fe4a0e379d5bf02cfd5b1c26c1b
Alex Renyov <limpalex@gmail.com>
2014-01-14 00:03:33 -0800

    Hiding left ads block.

--- vk_repost_remover.js
+++ vk_repost_remover.js
@@ -1,16 +1,18 @@
 // ==UserScript==
-// @name       Repost remover
-// @namespace  http://alex.renyov.vk/
-// @version    0.1
-// @description  enter something useful
-// @match      http://vk.com/feed
-// @copyright  2014, Alex Renyov
-// @require    http://code.jquery.com/jquery-1.10.2.min.js
+// @name         Repost remover
+// @namespace    http://alex.renyov.vk/
+// @version      0.2
+// @description  cleans vk news garbage
+// @match        http://vk.com/feed
+// @copyright    2014, Alex Renyov
+// @require      http://code.jquery.com/jquery-1.10.2.min.js
 // ==/UserScript==
 
 $(function() {
     window.setInterval(function() {
         $('[class*="repost"]').remove();
         $('.feed_row').has('.group_share').remove();
+        // do not remove left_ads entirely, as it leads to vk javascript errors
+        $('#left_ads').css('display', 'none');
     }, 1000);
 });
